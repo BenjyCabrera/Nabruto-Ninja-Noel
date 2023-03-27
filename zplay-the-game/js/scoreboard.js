2b3f8273c5809d5d4c5495fcdb153f0895d4ebd5
BenjyCabrera <benjamin.cabrera.arroyo89@gmail.com>
2023-03-27 20:30:45 +0200

    score cambiado

--- zplay-the-game/js/scoreboard.js
+++ zplay-the-game/js/scoreboard.js
@@ -1,5 +1,6 @@
 const ScoreBoard = {
 	ctx: undefined,
+	lastScore: 0,
 
 	init(ctx) {
 		this.ctx = ctx;
@@ -9,9 +10,22 @@ const ScoreBoard = {
 		score = Math.floor(score);
 
 		this.ctx.save();
-		this.ctx.font = '30px Comic Sans MS';
-		this.ctx.fillStyle = 'green';
-		this.ctx.fillText(score, 50, 50);
+
+		this.ctx.fillStyle = '#000';
+		this.ctx.fillRect(0, 0, 150, 70);
+
+		if (score > this.lastScore) {
+			this.ctx.font = '40px Arial';
+			this.ctx.fillStyle = 'yellow';
+		} else {
+			this.ctx.font = '30px Arial';
+			this.ctx.fillStyle = 'white';
+		}
+
+		this.ctx.fillText(score, 20, 50);
+
 		this.ctx.restore();
+
+		this.lastScore = score;
 	},
 };
